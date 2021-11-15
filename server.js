70380b39957849dc20be0725c9a3ed9bf08df7de
charliepoker <iheanachocharlie@gmail.com>
2021-11-15 15:41:34 +0100

    modified server

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ require("dotenv").config();
 require("express-async-errors");
 
 // extra security packages
-const helmet = require('helmet');
-const cors = require('cors');
-const xss = require('xss-clean');
-const rateLimiter = require('express-rate-limit');
-
-
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 
 const express = require("express");
 const app = express();
@@ -23,8 +21,25 @@ const jobsRouter = require("./routes/jobs.routes");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 app.use(express.json());
 // extra packages
+
+// simple route
+app.get("/", (req, res) => {
+  res.send("<h1>Jobs Api</h1>");
+});
 
 // routes
 app.use("/api/V1/auth", authRouter);
@@ -37,7 +52,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
