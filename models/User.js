e2a266cc5c4ade03b1b8e41b9f1497fbe03f2d12
charliepoker <iheanachocharlie@gmail.com>
2021-11-19 11:34:30 +0100

    modified email validation

--- models/User.js
+++ models/User.js
@@ -12,10 +12,7 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "Please provide email"],
-    match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Please provide a valid email",
-    ],
+  
     unique: true,
   },
   password: {
@@ -42,6 +39,13 @@ UserSchema.methods.createJWT = function () {
     }
   );
 };
+
+// validate email
+UserSchema.path("email").validate(function (email) {
+  var emailRegex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return emailRegex.test(email.text);
+}, "Please provide a valid email");
 
 
 // create an instance to compare password
